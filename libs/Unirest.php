34fce300205d0639c05e35d7a407a835968799c5
Melroy van den Berg <webmaster@web-share.nl>
2016-03-27 13:48:52 +0200

    Using latest Unirest version, fixing memory leak, adding error output

--- libs/Unirest.php
+++ libs/Unirest.php
@@ -1,7 +1,7 @@
 <?php
 
-require(dirname(__FILE__) . '/Unirest/HttpMethod.php');
-require(dirname(__FILE__) . '/Unirest/HttpResponse.php');
-require(dirname(__FILE__) . '/Unirest/Unirest.php');
-
-?>
+require_once dirname(__FILE__) . '/Unirest/Exception.php';
+require_once dirname(__FILE__) . '/Unirest/Method.php';
+require_once dirname(__FILE__) . '/Unirest/Response.php';
+require_once dirname(__FILE__) . '/Unirest/Request.php';
+require_once dirname(__FILE__) . '/Unirest/Request/Body.php';
--- metacritic.php
+++ metacritic.php
@@ -17,7 +17,7 @@ class MetacriticAPI
         $game_name = preg_replace('/[^a-z\d\?!\-]/', '', $game_name);
 
         # Get the webpage
-        $response = Unirest::get("http://www.metacritic.com/game/pc/" . $game_name);                            
+        $response = Unirest\Request::get("http://www.metacritic.com/game/pc/" . $game_name, $headers = array(), $parameters = null);                            
         if($response->code == 200)
         {
             $returnValue = $response->raw_body;
@@ -31,6 +31,7 @@ class MetacriticAPI
         $html = str_get_html($page_response);
         # Define json output array
         $json_output = array();
+		$error = false;
         # init all vars
         $name = "";
         $metascritic_score = 0;
@@ -42,67 +43,82 @@ class MetacriticAPI
         $release_date = "";
         $image_url = "";
         $cheat_url = "";
-        foreach($html->find('div[class=product_title] span[itemprop=name]') as $element) 
-        {
-            $name = trim($element->plaintext);
-        }
-        
-        foreach($html->find('span[itemprop=ratingValue]') as $element) 
-        {
-            $metascritic_score = intval($element->plaintext);
-        }
-        
-        foreach($html->find("div[class=userscore_wrap] a[class=metascore_anchor] div[class=metascore_w]") as $element) 
-        {
-            $user_score = floatval($element->plaintext);
-        }
-        
-        foreach($html->find('span[itemprop=contentRating]') as $element) 
-        {
-            $rating = trim($element->plaintext);
-        }
-        
-        foreach($html->find('span[itemprop=genre]') as $element) 
-        {
-            $genres = trim($element->plaintext);
-        }        
-        
-        foreach($html->find('li[class=summary_detail developer] span[class=data]') as $element) 
-        {
-            $developer = trim($element->plaintext);
-        }
-        
-        foreach($html->find('li[class=summary_detail publisher] span[itemprop=name]') as $element) 
-        {
-            $publisher = trim($element->plaintext);
-        }
-        
-        foreach($html->find('span[itemprop=datePublished]') as $element) 
-        {
-            $release_date = trim($element->plaintext);
-        }
-        
-        foreach($html->find('img[itemprop=image]') as $element) 
-        {
-            $image_url = $element->src;
-        }
-        
-        foreach($html->find('li[class=summary_detail product_cheats] span[class=data] a') as $element) 
-        {
-            $cheat_url = $element->href;
-        }                                                            
-     
-        # Fill-in the array
-        $json_output['name'] = $name;
-        $json_output['metascritic_score'] = $metascritic_score;
-        $json_output['users_score'] = $user_score;
-        $json_output['rating'] = $rating;
-        $json_output['genres'] = $genres;
-        $json_output['developers'] = $developer;
-        $json_output['publishers'] = $publisher;
-        $json_output['release_date'] = $release_date;
-        $json_output['thumbnail_url'] = $image_url;
-        $json_output['cheat_url'] = $cheat_url;
+
+		if(!$html) 
+		{
+			$json_output['error'] = "Page could not be loaded!";
+			$error = true;
+		}
+
+		if(!$error)
+		{
+		    foreach($html->find('div[class=product_title] span[itemprop=name]') as $element) 
+		    {
+		        $name = trim($element->plaintext);
+		    }
+		    
+		    foreach($html->find('span[itemprop=ratingValue]') as $element) 
+		    {
+		        $metascritic_score = intval($element->plaintext);
+		    }
+		    
+		    foreach($html->find("div[class=userscore_wrap] a[class=metascore_anchor] div[class=metascore_w]") as $element) 
+		    {
+		        $user_score = floatval($element->plaintext);
+		    }
+		    
+		    foreach($html->find('span[itemprop=contentRating]') as $element) 
+		    {
+		        $rating = trim($element->plaintext);
+		    }
+		    
+		    foreach($html->find('span[itemprop=genre]') as $element) 
+		    {
+		        $genres = trim($element->plaintext);
+		    }        
+		    
+		    foreach($html->find('li[class=summary_detail developer] span[class=data]') as $element) 
+		    {
+		        $developer = trim($element->plaintext);
+		    }
+		    
+		    foreach($html->find('li[class=summary_detail publisher] span[itemprop=name]') as $element) 
+		    {
+		        $publisher = trim($element->plaintext);
+		    }
+		    
+		    foreach($html->find('span[itemprop=datePublished]') as $element) 
+		    {
+		        $release_date = trim($element->plaintext);
+		    }
+		    
+		    foreach($html->find('img[itemprop=image]') as $element) 
+		    {
+		        $image_url = $element->src;
+		    }
+		    
+		    foreach($html->find('li[class=summary_detail product_cheats] span[class=data] a') as $element) 
+		    {
+		        $cheat_url = $element->href;
+		    }
+	
+			# Prevent memory leak
+			$html->clear();
+			unset($html);                                                      
+		 
+		    # Fill-in the array
+		    $json_output['name'] = $name;
+		    $json_output['metascritic_score'] = $metascritic_score;
+		    $json_output['users_score'] = $user_score;
+		    $json_output['rating'] = $rating;
+		    $json_output['genres'] = $genres;
+		    $json_output['developers'] = $developer;
+		    $json_output['publishers'] = $publisher;
+		    $json_output['release_date'] = $release_date;
+		    $json_output['thumbnail_url'] = $image_url;
+		    $json_output['cheat_url'] = $cheat_url;
+		}
+
         # Return JSON format
         return json_encode($json_output);
     }
